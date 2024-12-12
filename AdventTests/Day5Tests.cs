98fd5b87c16e5bbc86c27fba82b2d52976abb185
arborelia <gh@arborelia.net>
2024-12-12 15:28:27 -0500

    day 5 progress

---
+++ AdventTests/Day5Tests.cs
@@ -0,0 +1,51 @@
+﻿using Day5;
+
+namespace AdventTests
+{
+    [TestClass]
+    public sealed class Day5Tests
+    {
+        public static readonly string testPQ = @"
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+        ".Trim();
+        public static readonly PrintQueueOrganizer pq = PrintQueueOrganizer.FromLines(testPQ.Split("\n").ToList());
+
+        [TestMethod]
+        public void TestDependencies()
+        {
+            var depsOf13 = pq.GetDepsBefore(13);
+            Assert.IsTrue(depsOf13.Contains(47));
+            Assert.IsFalse(depsOf13.Contains(46));
+
+            var depsOf14 = pq.GetDepsBefore(14);
+            Assert.AreEqual(0, depsOf14.Count);
+        }
+    }
+}
---
+++ Day5/Day5.cs
@@ -0,0 +1,60 @@
+﻿// solution to: https://adventofcode.com/2024/day/4
+namespace Day5
+{
+    public record struct PrintQueueOrganizer(Dictionary<int, SortedSet<int>> Deps, List<List<int>> Orderings)
+    {
+        public static PrintQueueOrganizer Empty()
+        {
+            return new PrintQueueOrganizer([], []);
+        }
+
+        public static PrintQueueOrganizer FromLines(List<string> lines)
+        {
+            var pq = PrintQueueOrganizer.Empty();
+            pq.AddFromLines(lines);
+            return pq;
+        }
+
+        public void AddDependency(int before, int after)
+        {
+            if (!Deps.ContainsKey(after))
+            {
+                Deps[after] = new SortedSet<int>();
+            }
+            Deps[after].Add(before);
+        }
+
+        public readonly SortedSet<int> GetDepsBefore(int after)
+        {
+            if (Deps.TryGetValue(after, out SortedSet<int>? value))
+            {
+                return value;
+            }
+            else return [];
+        }
+
+        public void AddFromLines(List<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                if (line.Contains('|'))
+                {
+                    var parts = line.Split('|');
+                    int before = int.Parse(parts[0]);
+                    int after = int.Parse(parts[1]);
+                    AddDependency(before, after);
+                }
+                else if (line.Contains(','))
+                {
+                    var orderList = from part in line.Split(',')
+                                    select int.Parse(part);
+                    Orderings.Add(orderList.ToList());
+                }
+            }
+        }
+
+        public static void Main()
+        {
+        }
+    }
+}
